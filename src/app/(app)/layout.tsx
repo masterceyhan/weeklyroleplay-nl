500f6a3a631bd577840873b60b9b6eedc6e304dc
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-12-23 14:53:31 +0100

    feat: add metadata

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,6 +6,53 @@ import Announcement from "@/components/home/announcement"
 import Header from "@/components/home/header"
 import Footer from "@/components/home/footer"
 import { Toaster } from "@/components/ui/sonner"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Weekly Roleplay",
+  description: "Waar je elke dag een nieuw verhaal kan beleven.",
+  keywords: [
+    "FiveM",
+    "Roleplay",
+    "Dutch",
+    "Nederlands",
+    "Weekly",
+    "Doneren",
+    "Ideal",
+    "Paypal",
+    "Solliciteren",
+    "Server",
+    "Politie",
+  ],
+  authors: [
+    {
+      name: "Thoo",
+      url: "https://vertexscripts.com",
+    },
+  ],
+  alternates: {
+    canonical: `https://www.weeklyroleplay.nl`,
+  },
+  creator: "Thoo",
+  openGraph: {
+    type: "website",
+    locale: "nl_NL",
+    url: "https://www.weeklyroleplay.nl",
+    title: "Weekly Roleplay",
+    description:
+      "Op onze website kunt u de status zien en is het mogelijk om te doneren/solliciteren en om meer te weten te komen over ons! Ook kunt u via hier onze server regels bekijken!",
+    siteName: "Weekly Roleplay",
+    images: [`https://www.weeklyroleplay.nl/logo.png`],
+  },
+  twitter: {
+    card: "summary",
+    title: "Weekly Roleplay",
+    description:
+      "Op onze website kunt u de status zien en is het mogelijk om te doneren/solliciteren en om meer te weten te komen over ons! Ook kunt u via hier onze server regels bekijken!",
+    images: [`https://www.weeklyroleplay.nl/logo.png`],
+    creator: "@thoo",
+  },
+}
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
